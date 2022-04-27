2b19c1328b57d663e438d2803323dcadfc1a1988
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2022-04-27 12:32:07 -0500

    Update footer

--- components/footer/Footer.js
+++ components/footer/Footer.js
@@ -1,4 +1,4 @@
-import { Box, VStack, Stack, Text, Center, Link } from '@chakra-ui/react'
+import { chakra, Flex, Box, Link } from '@chakra-ui/react'
 import { FaInstagram, FaTwitter, FaGithub } from 'react-icons/fa'
 import { FiExternalLink } from 'react-icons/fi'
 
@@ -6,32 +6,43 @@ import SocialButton from './SocialButton'
 
 export default function Footer() {
   return (
-    <Box pt={20} pb={10}>
-      <Center>
-        <VStack spacing={5}>
-          <Box>
-            <Text>
-              2022 ▽Volk&apos;s. Made with ♡ by{' '}
-              <Link href="https://github.com/Juan7diaz/volk-shop" isExternal>
-                Juan Diaz <FiExternalLink style={{ display: 'initial' }} />
-              </Link>
-            </Text>
-          </Box>
-          <Box>
-            <Stack direction={'row'} spacing={6}>
-              <SocialButton label={'Twitter'} href={'#'}>
-                <FaTwitter />
-              </SocialButton>
-              <SocialButton label={'YouTube'} href={'#'}>
-                <FaGithub />
-              </SocialButton>
-              <SocialButton label={'Instagram'} href={'#'}>
-                <FaInstagram />
-              </SocialButton>
-            </Stack>
-          </Box>
-        </VStack>
-      </Center>
-    </Box>
+    <Flex
+      w="full"
+      as="footer"
+      flexDir={{ base: 'column', sm: 'row' }}
+      align="center"
+      justify="space-between"
+      p="10"
+      px={'40px'}
+      _dark={{
+        bg: 'gray.800'
+      }}
+    >
+      <chakra.a
+        href="#"
+        fontSize="xl"
+        color="primary"
+        py={{ base: '0', xs: '100px' }}
+      >
+        V O L K &apos; S
+      </chakra.a>
+      <Box>
+        Made with ♡ by{' '}
+        <Link href="https://github.com/Juan7diaz/volk-shop" isExternal>
+          Juan Diaz <FiExternalLink style={{ display: 'initial' }} />
+        </Link>
+      </Box>
+      <Flex>
+        <SocialButton label={'Instagram'} href={'#'}>
+          <FaInstagram />
+        </SocialButton>
+        <SocialButton label={'Instagram'} href={'#'}>
+          <FaTwitter />
+        </SocialButton>
+        <SocialButton label={'Instagram'} href={'#'}>
+          <FaGithub />
+        </SocialButton>
+      </Flex>
+    </Flex>
   )
 }
