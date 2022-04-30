d7af09f82456980421caddfa45f8892d04704ac0
Juan Diaz Guerrero <juanluisdiazguerrero@gmail.com>
2022-04-29 20:50:27 -0500

    organize the data in the benefits section

--- components/benefits/Benefits.js
+++ components/benefits/Benefits.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { SimpleGrid, useColorModeValue } from '@chakra-ui/react'
-import { FaUserShield, FaGlobeAmericas, FaHandHoldingUsd } from 'react-icons/fa'
 
 import Feature from './Feature'
+import { benefits } from '../../data/benefits'
 
 export default function Benefits() {
   return (
@@ -14,24 +14,14 @@ export default function Benefits() {
       mx="auto"
       bg={useColorModeValue('white', 'gray.800')}
     >
-      <Feature title="Shopping security" icon={<FaUserShield />}>
-        There are many variations of passages of Lorem Ipsum available, but the
-        majority have suffered alteration in some form, by injected humour, or
-        randomised words which dont look even slightly believable
-      </Feature>
-
-      <Feature title="the best price" icon={<FaHandHoldingUsd />}>
-        Provide a seamless customer experience across channels, like reserving
-        online and picking up in store. Our SDKs make it easy to integrate into
-        your own mobile and web applications to accept in-store payments.
-      </Feature>
-
-      <Feature title="Shipping to all Latin America" icon={<FaGlobeAmericas />}>
-        Lorem Ipsum is not simply random text. It has roots in a piece of
-        classical Latin literature from 45 BC, making it over 2000 years old.
-        Richard McClintock, a Latin professor at Hampden-Sydney College in
-        Virginia
-      </Feature>
+      {benefits.map((benefit) => {
+        const { id, icon, title, desc } = benefit
+        return (
+          <Feature title={title} key={id} icon={icon}>
+            {desc}
+          </Feature>
+        )
+      })}
     </SimpleGrid>
   )
 }
---
+++ data/benefits.js
@@ -0,0 +1,22 @@
+import { FaUserShield, FaHandHoldingUsd, FaGlobeAmericas } from 'react-icons/fa'
+
+export const benefits = [
+  {
+    id: 1,
+    title: 'Shopping security',
+    desc: 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which dont look even slightly believable',
+    icon: <FaUserShield />
+  },
+  {
+    id: 2,
+    title: 'The best price',
+    desc: 'Provide a seamless customer experience across channels, like reserving online and picking up in store. Our SDKs make it easy to integrate into your own mobile and web applications to accept in-store payments.',
+    icon: <FaHandHoldingUsd />
+  },
+  {
+    id: 3,
+    title: 'Shipping to all Latin America',
+    desc: 'Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia',
+    icon: <FaGlobeAmericas />
+  }
+]
